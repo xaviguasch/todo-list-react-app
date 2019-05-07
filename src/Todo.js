f1ea2639e92e9708d63a8ab96c6f0a5e1b7ab7fe
xaviguasch <xaviguasch@gmail.com>
2019-05-07 12:08:25 +0200

    added EDIT to todos logic

--- src/Todo.js
+++ src/Todo.js
@@ -4,21 +4,59 @@ class Todo extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      isEditing: false,
+      task: this.props.task
+    }
+
     this.handleClick = this.handleClick.bind(this)
+    this.toggleForm = this.toggleForm.bind(this)
+    this.handleUpdate = this.handleUpdate.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleClick() {
     this.props.removeTodo(this.props.id)
   }
 
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing })
+  }
+
+  handleUpdate(evt) {
+    evt.preventDefault()
+    this.props.updateTodo(this.props.id, this.state.task)
+    this.setState({ isEditing: false })
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
   render() {
-    return (
-      <div>
-        <li>{this.props.task}</li>
-        <button>Edit</button>
-        <button onClick={this.handleClick}>X</button>
-      </div>
-    )
+    let result
+    if (this.state.isEditing) {
+      result = (
+        <div>
+          <form onSubmit={this.handleUpdate}>
+            <input type='text' value={this.state.task} name='task' onChange={this.handleChange} />
+            <button>Save</button>
+          </form>
+        </div>
+      )
+    } else {
+      result = (
+        <div>
+          <li>{this.props.task}</li>
+          <button onClick={this.toggleForm}>Edit</button>
+          <button onClick={this.handleClick}>X</button>
+        </div>
+      )
+    }
+
+    return result
   }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -10,6 +10,7 @@ class TodoList extends Component {
     }
     this.create = this.create.bind(this)
     this.remove = this.remove.bind(this)
+    this.update = this.update.bind(this)
   }
 
   create(newTodo) {
@@ -24,9 +25,23 @@ class TodoList extends Component {
     })
   }
 
+  update(id, updatedTask) {
+    const updatedTodos = this.state.todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, task: updatedTask }
+      }
+      return todo
+    })
+    this.setState({
+      todos: updatedTodos
+    })
+  }
+
   render() {
     const todos = this.state.todos.map(todo => {
-      return <Todo task={todo.task} key={todo.id} id={todo.id} removeTodo={this.remove} />
+      return (
+        <Todo task={todo.task} key={todo.id} id={todo.id} removeTodo={this.remove} updateTodo={this.update} />
+      )
     })
     return (
       <div>
